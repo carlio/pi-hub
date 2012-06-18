1df2967bba14fcaa43ce9ed6eff177abcaf429c8
carlio <git@carlcrowder.com>
2012-06-18 21:36:18 +0200

    Removing release from admin fields on non-local dev

--- pihub/packages/admin.py
+++ pihub/packages/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from pihub.packages.models import Release, Pkg, ReleaseData, ReleaseUrl,\
     MirrorState
+from django.conf import settings
 
 class FieldHashAdmin(admin.ModelAdmin):
-    readonly_fields = ('field_hash', )
+    readonly_fields = ('field_hash', ) + ( () if settings.ADMIN_EDIT_RELEASE else ('release',) )
 
 admin.site.register(Pkg)
 admin.site.register(Release)
